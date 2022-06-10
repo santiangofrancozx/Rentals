b5964ea169e2337fa77c62cf2b90f8b597904650
Santiago Franco <santiagofrancozx@gmail.com>
2022-06-10 15:58:17 -0500

    Jun 5 7:32, classes

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/user1/Desktop/ProyectoX
+# Install script for directory: /Users/user1/Desktop/ProyectoX vF
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/user1/Desktop/ProyectoX/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/user1/Desktop/ProyectoX vF/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
