30d90808c665a706c97e806930e56341ec488037
Santiago Franco <santiagofrancozx@gmail.com>
2022-06-18 16:11:44 -0500

    Version final, no se pongan a subir mamadas conio

---
+++ cmake-build-debug/CMakeFiles/ProyectoX.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/user1/Desktop/Rentals/Autos/Comfort.cpp" "CMakeFiles/ProyectoX.dir/Autos/Comfort.cpp.o" "gcc" "CMakeFiles/ProyectoX.dir/Autos/Comfort.cpp.o.d"
+  "/Users/user1/Desktop/Rentals/Autos/Sport.cpp" "CMakeFiles/ProyectoX.dir/Autos/Sport.cpp.o" "gcc" "CMakeFiles/ProyectoX.dir/Autos/Sport.cpp.o.d"
+  "/Users/user1/Desktop/Rentals/Autos/Work.cpp" "CMakeFiles/ProyectoX.dir/Autos/Work.cpp.o" "gcc" "CMakeFiles/ProyectoX.dir/Autos/Work.cpp.o.d"
+  "/Users/user1/Desktop/Rentals/Main/main.cpp" "CMakeFiles/ProyectoX.dir/Main/main.cpp.o" "gcc" "CMakeFiles/ProyectoX.dir/Main/main.cpp.o.d"
+  "/Users/user1/Desktop/Rentals/Users/Admin.cpp" "CMakeFiles/ProyectoX.dir/Users/Admin.cpp.o" "gcc" "CMakeFiles/ProyectoX.dir/Users/Admin.cpp.o.d"
+  "/Users/user1/Desktop/Rentals/Users/Provider.cpp" "CMakeFiles/ProyectoX.dir/Users/Provider.cpp.o" "gcc" "CMakeFiles/ProyectoX.dir/Users/Provider.cpp.o.d"
+  "/Users/user1/Desktop/Rentals/Users/Subscribed.cpp" "CMakeFiles/ProyectoX.dir/Users/Subscribed.cpp.o" "gcc" "CMakeFiles/ProyectoX.dir/Users/Subscribed.cpp.o.d"
+  "/Users/user1/Desktop/Rentals/Users/User.cpp" "CMakeFiles/ProyectoX.dir/Users/User.cpp.o" "gcc" "CMakeFiles/ProyectoX.dir/Users/User.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ProyectoX.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProyectoX.dir/Autos/Comfort.cpp.o"
+  "CMakeFiles/ProyectoX.dir/Autos/Comfort.cpp.o.d"
+  "CMakeFiles/ProyectoX.dir/Autos/Sport.cpp.o"
+  "CMakeFiles/ProyectoX.dir/Autos/Sport.cpp.o.d"
+  "CMakeFiles/ProyectoX.dir/Autos/Work.cpp.o"
+  "CMakeFiles/ProyectoX.dir/Autos/Work.cpp.o.d"
+  "CMakeFiles/ProyectoX.dir/Main/main.cpp.o"
+  "CMakeFiles/ProyectoX.dir/Main/main.cpp.o.d"
+  "CMakeFiles/ProyectoX.dir/Users/Admin.cpp.o"
+  "CMakeFiles/ProyectoX.dir/Users/Admin.cpp.o.d"
+  "CMakeFiles/ProyectoX.dir/Users/Provider.cpp.o"
+  "CMakeFiles/ProyectoX.dir/Users/Provider.cpp.o.d"
+  "CMakeFiles/ProyectoX.dir/Users/Subscribed.cpp.o"
+  "CMakeFiles/ProyectoX.dir/Users/Subscribed.cpp.o.d"
+  "CMakeFiles/ProyectoX.dir/Users/User.cpp.o"
+  "CMakeFiles/ProyectoX.dir/Users/User.cpp.o.d"
+  "ProyectoX"
+  "ProyectoX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProyectoX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
