37096169141f8fe05e3d3312489e8d7b24eaecf8
Valeria <98919981+vzvval@users.noreply.github.com>
2022-06-20 22:44:44 -0500

    adding polimorfismo

--- Autos/Comfort.cpp
+++ Autos/Comfort.cpp
@@ -9,6 +9,7 @@ using namespace::std;
 
 Comfort::Comfort() {
     numberSeats = numberAirBag = 0;
+    subtotal = 0;
 }
 
 Comfort::~Comfort() {
@@ -23,6 +24,12 @@ void Comfort::setNumberSeats(){
 void Comfort::setNumberAirBag(){
     cout<<"Number of air bags: ";
     cin>>numberAirBag;
+}
+
+void Comfort::Price(){
+    cout<<"Set price: ";
+    cin >> subtotal;
+    price = (subtotal * 0.5) + subtotal;
 }
 
 int Comfort::getNumberSeats(){
